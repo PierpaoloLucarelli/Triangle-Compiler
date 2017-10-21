5dc34b48390f86f1957716305521d4b4755bd3f7
Pierpaolo <plucarelli.94@gmail.com>
2017-10-21 10:35:22 +0100

    first commit

---
+++ TriangleCompiler/Compiler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TriangleCompiler
+{
+    SourceFile _source;
+
+    class Compiler
+    {
+
+        Compiler(string sourceFileName)
+        {
+            _source = new SourceFile(sourceFileName);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Running Triangle Compiler");
+            var sourceFileName = args[0];
+            if (sourceFileName != null)
+            {
+                var compiler = new Compiler(sourceFileName);
+            }
+        }
+    }
+}
