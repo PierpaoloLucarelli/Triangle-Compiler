3fe5f6a8d40c58424691ffa50853ab8a44163a52
Pierpaolo <plucarelli.94@gmail.com>
2017-12-08 10:21:53 +0000

    working with put()

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Triangle-Compiler
+# Triangle Tools in C#
+
+This is a C# version of the Triangle Tools (compiler, disassembler and interpreter) used in the textbook:
+
+"Programming Language Processors in Java" by D.A. Watt and D.F. Brown, Pearson (2000).
+
---
+++ Triangle.Compiler/SyntacticAnalyzer/Parser - Declarations.cs
@@ -0,0 +1,103 @@
+using Triangle.Compiler.SyntaxTrees.Declarations;
+using Triangle.Compiler.SyntaxTrees.Terminals;
+using Triangle.Compiler.SyntaxTrees.Expressions;
+using Triangle.Compiler.SyntaxTrees.Types;
+
+namespace Triangle.Compiler.SyntacticAnalyzer
+{
+    public partial class Parser
+    {
+
+        ///////////////////////////////////////////////////////////////////////////////
+        //
+        // DECLARATIONS
+        //
+        ///////////////////////////////////////////////////////////////////////////////
+
+        /**
+         * Parses the declaration, and constructs an AST to represent its phrase
+         * structure.
+         * 
+         * @return a {@link triangle.compiler.syntax.trees.declarations.Declaration}
+         * 
+         * @throws SyntaxError
+         *           a syntactic error
+         * 
+         */
+        Declaration ParseDeclaration()
+        {
+            Declaration declaration = null;
+            var startLocation = _currentToken.Start;
+            declaration = ParseSingleDeclaration();
+            while (_currentToken.Kind == TokenKind.Semicolon)
+            {
+                AcceptIt();
+                Declaration decl = ParseSingleDeclaration();
+                var declarationPosition = new SourcePosition(startLocation, _currentToken.Finish);
+                declaration = new SequentialDeclaration(declaration, decl, declarationPosition);
+            }
+            return declaration;
+        }
+
+        /**
+         * Parses the single declaration, and constructs an AST to represent its
+         * phrase structure.
+         * 
+         * @return a {@link triangle.compiler.syntax.trees.declarations.Declaration}
+         * 
+         * @throws SyntaxError
+         *           a syntactic error
+         * 
+         */
+        Declaration ParseSingleDeclaration()
+        {
+            Declaration declaration = null;
+            var startLocation = _currentToken.Start;
+            switch (_currentToken.Kind)
+            {
+
+                case TokenKind.Const:
+                    {
+                        AcceptIt();
+                        Identifier identifier = ParseIdentifier();
+                        Accept(TokenKind.Is);
+                        Expression expression = ParseExpression();
+                        var declarationPosition = new SourcePosition(startLocation, _currentToken.Finish);
+                        declaration = new ConstDeclaration(identifier, expression, declarationPosition);
+                        break;
+                    }
+
+                case TokenKind.Var:
+                    {
+                        AcceptIt();
+                        Identifier identifier = ParseIdentifier();
+                        Accept(TokenKind.Colon);
+                        TypeDenoter typeDenoter = ParseTypeDenoter();
+                        var declarationPosition = new SourcePosition(startLocation, _currentToken.Finish);
+                        declaration = new VarDeclaration(identifier, typeDenoter, declarationPosition);
+                        break;
+                    }
+
+
+                    // is this extra?
+                case TokenKind.Type:
+                    {
+                        AcceptIt();
+                        Identifier identifier = ParseIdentifier();
+                        Accept(TokenKind.Is);
+                        TypeDenoter typeDenoter = ParseTypeDenoter();
+                        var declarationPosition = new SourcePosition(startLocation, _currentToken.Finish);
+                        declaration = new TypeDeclaration(identifier, typeDenoter, declarationPosition);
+                        break;
+                    }
+
+                default:
+                    {
+                        RaiseSyntacticError("\"%\" cannot start a declaration", _currentToken);
+                        break;
+                    }
+            }
+            return declaration;
+        }
+    }
+}
